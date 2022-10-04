d9c980bfcb32dd32a561b67e476ffc96f5fd8f06
TmatzXzonEv <tmatzxzone42@gmail.com>
2022-10-04 08:52:33 +0800

    Update direct_link_generator.py

--- bot/helper/mirror_utils/download_utils/direct_link_generator.py
+++ bot/helper/mirror_utils/download_utils/direct_link_generator.py
@@ -341,7 +341,7 @@ def krakenfiles(page_link: str) -> str:
     }
 
     dl_link_resp = rsession().post(
-        f"https://kemono.party/", headers=headers)
+        f"{page_link}", headers=headers)
 
     if "url" in dl_link_resp:
         return dl_link_resp["url"]
