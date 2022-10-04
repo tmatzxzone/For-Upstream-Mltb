ca2addec76314963b9378e4b8ebee01521148649
TmatzXzonEv <tmatzxzone42@gmail.com>
2022-10-04 08:31:49 +0800

    Update direct_link_generator.py

--- bot/helper/mirror_utils/download_utils/direct_link_generator.py
+++ bot/helper/mirror_utils/download_utils/direct_link_generator.py
@@ -333,7 +333,8 @@ def krakenfiles(page_link: str) -> str:
     Based on https://github.com/tha23rd/py-kraken
     By https://github.com/junedkh """
     page_resp = rsession().get(page_link)
-
+    soup = BeautifulSoup(page_resp.text, "lxml")
+    
     hashes = [
         item["data-file-hash"]
         for item in soup.find_all("div", attrs={"data-file-hash": True})
